f30238fa4b105d15507ed2d7a3c4328ed5f8e742
William Bruntrager <w.bruntrager@gmail.com>
2026-01-10 12:14:47 +0100

    Tweak backend

--- crossword-backend/ecosystem.config.cjs
+++ crossword-backend/ecosystem.config.cjs
@@ -4,6 +4,8 @@ module.exports = {
       name: 'crossword-backend',
       script: 'index.ts',
       interpreter: '/home/william/.bun/bin/bun',
+      interpreter_args: 'run',
+      exec_mode: 'fork',
       env: {
         NODE_ENV: 'development',
         PORT: 8921,
