d74a7cb2331d619234d086023c0882c85a171e60
William Bruntrager <w.bruntrager@gmail.com>
2026-01-12 10:21:37 +0000

    Puzzle export

--- crossword-backend/export-puzzles.js
+++ crossword-backend/export-puzzles.js
@@ -1,8 +1,16 @@
 import db from './db-knex'
 
 const exportPuzzles = async () => {
-  const puzzles = await db('puzzles').select('*').whereIn('id', [6, 7])
-  console.log(puzzles)
+  const puzzles = await db('puzzles').select('*')
+  // console.log(puzzles)
+
+  for (const puzzle of puzzles) {
+    console.log(puzzle.title)
+    console.log('----')
+    console.log(puzzle.grid)
+    console.log('----')
+    console.log(puzzle.clues)
+  }
 }
 
 exportPuzzles().then(() => {
