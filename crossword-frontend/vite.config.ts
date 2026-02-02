8437ccc38ac240a71a68b6c8c5d103b98b26de85
William Bruntrager <w.bruntrager@gmail.com>
2026-02-02 08:59:18 +0100

    Use aliased imports with `@`

--- crossword-frontend/vite.config.ts
+++ crossword-frontend/vite.config.ts
@@ -2,9 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { VitePWA } from 'vite-plugin-pwa'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   plugins: [
     react(),
     tailwindcss(),
